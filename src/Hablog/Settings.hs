6b06d5892a821ad6f45777d195cb7241c4ec51f6
suppi <soupiral@gmail.com>
2015-08-07 06:36:25 -0400

    updating highlight.js themes

--- src/Hablog/Settings.hs
+++ src/Hablog/Settings.hs
@@ -6,19 +6,19 @@ import Data.Text.Lazy (Text)
 import Text.Blaze.Internal (AttributeValue)
 
 blogTitle :: Text
-blogTitle = "Hablog"
+blogTitle = "λm"
 
 blogTheme :: Theme
-blogTheme = darkTheme
+blogTheme = lightTheme
 
 
 blogPort :: Int
-blogPort = 8080
+blogPort = 80
 
 data Theme = Theme { bgTheme :: AttributeValue, codeTheme :: AttributeValue }
 
 darkTheme :: Theme
-darkTheme = Theme "static/css/dark.css" "static/highlight/styles/obsidian.css"
+darkTheme = Theme "static/css/dark.css" "static/highlight/styles/hybrid.css"
 
 lightTheme :: Theme
-lightTheme  = Theme "static/css/light.css" "static/highlight/styles/solarized_light.css"
+lightTheme  = Theme "static/css/light.css" "static/highlight/styles/docco.css"
