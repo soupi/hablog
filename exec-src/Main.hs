1660893688b2a2543d9aad2038e67e4f0c8a0890
suppi <soupiral@gmail.com>
2015-10-03 21:09:16 +0300

    run with tls, untested

--- exec-src/Main.hs
+++ exec-src/Main.hs
@@ -2,15 +2,30 @@
 
 module Main where
 
+import Control.Concurrent (forkIO)
 import System.Environment (getArgs)
-import Hablog.Run (run)
+import Hablog.Run (run, runTLS)
 import Hablog.Settings (blogPort)
 
 main :: IO ()
-main = do
+main =
   getArgs >>= \case
     [] ->
       run blogPort
+
+    ["both", portStr, tlsPortStr, key, cert] ->
+      case (reads portStr, reads tlsPortStr) of
+        ([(port, "")], [(tlsPort, "")]) -> do
+            _ <- forkIO $ run port
+            runTLS tlsPort key cert
+        _ ->
+            putStrLn usageMsgBoth
+    ["tls", portStr, key, cert] ->
+      case reads portStr of
+        [(port, "")] ->
+          runTLS port key cert
+        _ ->
+          putStrLn usageMsgTLS
     [portStr] ->
       case reads portStr of
         [(port, "")] ->
@@ -21,4 +36,18 @@ main = do
       putStrLn usageMsg
 
 usageMsg :: String
-usageMsg = "Usage: hablog [<port>]"
+usageMsg =
+  unlines
+    ["Usage: hablog <port>"
+    ,"or"
+    ,usageMsgTLS
+    ,"or"
+    ,usageMsgBoth
+    ]
+
+usageMsgTLS :: String
+usageMsgTLS = "Usage: hablog tls <port> <key file> <cert file>"
+
+usageMsgBoth :: String
+usageMsgBoth = "Usage: hablog both <port> <port tls> <key file> <cert file>"
+
